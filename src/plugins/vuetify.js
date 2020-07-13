37302ea811f2f039a86d6eec11785509718374ff
D3S0X <24937357+D3S0X@users.noreply.github.com>
2020-07-13 22:33:24 +0200

    Use local Roboto font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,4 +1,5 @@
 import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
+import "roboto-fontface/css/roboto/roboto-fontface.css";
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
